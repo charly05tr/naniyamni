bdd865506438fd1ee830068f6ab443e84c8444e8
charly05tr <charlyst23dv@gmail.com>
2025-09-27 17:20:54 -0600

    url

--- naniyamni_app/src/features/oferta-turistica/proveedor/services/postReserva.js
+++ naniyamni_app/src/features/oferta-turistica/proveedor/services/postReserva.js
@@ -2,6 +2,7 @@ import { API_URL } from "@config";
 
 export const postReserva = async (reserva, tipo) => {
     console.log(tipo);
+    console.log(reserva);
     const token = localStorage.getItem('token');
     const response = await fetch(`${API_URL}reservas/?tipo=${tipo}`, {
         method: "POST",
